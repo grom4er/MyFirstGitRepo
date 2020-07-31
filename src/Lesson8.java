17b3ee33c0d48a4ad73f2ada327f2199f1085202
Tonitrus <grom4er@gmail.com>
2020-07-31 05:10:16 +0300

    Add Lesson 8, task 1

---
+++ src/Lesson8.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+//
+
+public class Lesson8 {
+    static Scanner scW = new Scanner(System.in);
+    static Scanner scN = new Scanner(System.in);
+    static String userChoice = null;
+    static int count = 1;
+    static int Subparagraph = 0;
+    static int userNumber;
+
+    static void parsingAskMethod(String MethodName, String userCase) {
+        if (userCase == null) {
+            System.out.printf("Do you want active method %s?" +
+                    "\nPlease press \"Y\" to accept OR Press " +
+                    "\"N\" to decline: \n", MethodName);
+        } else {
+            System.out.printf("Answer for %s is : \n", MethodName);
+        }
+    }
+
+    static String nameTask(int Task, int Subparagraph) {
+        return String.format("The task %d. Subparagraph %d",
+                Task, Subparagraph);
+    }
+
+    static boolean YesorNo(String userChoice) {
+        userChoice = userChoice.toUpperCase();
+        switch (userChoice) {
+            case "Y":
+                System.out.println("You answer is Y. I active method for you.");
+                return true;
+            case "N":
+                System.out.println("You answer is N. Let's go another.");
+                return false;
+            default:
+                System.out.printf("You answer %s is wrong. " +
+                        "\nMust be only Y or N.\n" +
+                        "Please, try again.", userChoice);
+                System.out.println();
+        }
+        userChoice = scW.nextLine();
+        return YesorNo(userChoice);
+    }
+
+    static void printDigits() {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i + "  " + ++i);
+        }
+    }
+
+    static int sumOfDigits(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        System.out.printf("Sum from 1 to %d, is: \n", number);
+        return sum; //IntStream.range(1,number+1).sum()
+    }
+
+    static int checkDigit() {
+        boolean temp = scN.hasNextInt();
+        if (temp) {
+            return scN.nextInt();
+        }
+        System.out.println("Sorry, it not int number. Try again!");
+        scN.next();
+        return checkDigit();
+    }
+
+    static void numbersDivisibleBy3(int from, int To) {
+        for (int i = from; i <= To; i++) {
+            if (i % 3 == 0) {
+                System.out.printf("%d is divisible by 3 \n", i);
+            }
+        }
+    }
+
+    static void canItTringleBe(int firstSide, int secondSide, int thirdSide) {
+
+        if (firstSide + secondSide < thirdSide) {
+            System.out.println("It Tringle not impossible");
+            return;
+        } else if (firstSide + thirdSide < secondSide) {
+            System.out.println("It Tringle not impossible");
+            return;
+        } else if (secondSide + thirdSide < firstSide) {
+            System.out.println("It Tringle not impossible");
+            return;
+        } else if (thirdSide + secondSide < firstSide) {
+            System.out.println("It Tringle not impossible");
+            return;
+        } else {
+            System.out.println("It Tringle is impossible");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //task 1
+        System.out.println("Task 1");
+        while (count < 4) {
+            userChoice = null;
+            parsingAskMethod(nameTask(1, ++Subparagraph), userChoice);
+            userChoice = scW.nextLine();
+            if (YesorNo(userChoice)) {
+                switch (count) {
+                    case 1:
+                        printDigits();
+                        break;
+                    case 2:
+                        System.out.println("You want sum from 1 to  what number?");
+                        System.out.println(sumOfDigits(checkDigit()));
+                        break;
+                    case 3:
+                        System.out.println("You want to find digit's by 3!");
+                        System.out.println("Please, write first number from what digit start:");
+                        int fNum = checkDigit();
+                        System.out.println("Now, write second number, to what digit over:");
+                        int sNum = checkDigit();
+                        numbersDivisibleBy3(fNum, sNum);
+                        break;
+                    case 4:
+                        System.out.println("Let we check, can be it Tringle!");
+                        System.out.println("Please, write first number :");
+                        fNum = checkDigit();
+                        System.out.println("Please, write second number :");
+                        sNum = checkDigit();
+                        System.out.println("Please, write third number :");
+                        int tNum = checkDigit();
+                        canItTringleBe(fNum,sNum,tNum);
+                }
+            }
+            count++;
+        }
+        System.out.println("______________________________");
+
+    }
+}
