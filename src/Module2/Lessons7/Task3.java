1f21fb02ce4a39467441dd28de58b964a1ef4fe8
Tonitrus <grom4er@gmail.com>
2020-10-01 20:37:02 +0300

    Full update

--- src/Module2/Lessons7/Task3.java
+++ src/Module2/Lessons7/Task3.java
@@ -5,8 +5,8 @@ class TestGarage {
         Audi car = new Audi(10, "v12", 200);
         MercedesBenz truck = new MercedesBenz(15, "V20", 200);
         VolgaBus bus = new VolgaBus(20, "v16", 20);
-//        MiniGarage miniGarage = new MiniGarage();
-//        Garage garage = new Garage();
+        MiniGarage miniGarage = new MiniGarage();
+        MidGarage garage = new MidGarage();
 //        BigGarage bigGarage = new BigGarage();
 
 //test value - mini Garage
