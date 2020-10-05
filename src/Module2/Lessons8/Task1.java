bc36902b838a3c3802e3e56a9e04389ee3124e23
Tonitrus <grom4er@gmail.com>
2020-10-05 15:56:27 +0300

    Lesson 8, task 1 update

--- src/Module2/Lessons8/Task1.java
+++ src/Module2/Lessons8/Task1.java
@@ -1,27 +1,22 @@
 package Module2.Lessons8;
 
+
 import java.util.Arrays;
 
 class Computer {
-    private ComputePart computePart;
+
+    private ComputePart[] computePart;
     private int price;
     private ComputerDevice[] computeDevice;
 
-    @Override
-    public String toString() {
-        return "Computer{" +
-                "computePart=" + computePart +
-                ", price=" + price +
-                ", computeDevice=" + Arrays.toString(computeDevice) +
-                '}';
-    }
 
     Computer(int price) {
         this.price = price;
-        computePart = new ComputePart("MSI A88XM-E35 V2",
-                "INTEL Core i9-9900K ",
-                "Seagate BarraCuda HDD 2TB 7200rpm 256MB 3.5 SATA III",
-                "PNY PCI-E Quadro RTX 6000 24GB DDR6");
+        computePart = new ComputePart[]
+                {new ComputePart("HDD"),
+                        new ComputePart("MotherBoard"),
+                        new ComputePart("CPU"),
+                        new ComputePart("Video Card")};
     }
 
 
@@ -32,13 +27,22 @@ class Computer {
     Computer(int price, ComputerDevice... computeDevice) {
         this.price = price;
         this.computeDevice = computeDevice;
-        computePart = new ComputePart("MSI A88XM-E35 V2",
-                "INTEL Core i9-9900K ",
-                "Seagate BarraCuda HDD 2TB 7200rpm 256MB 3.5 SATA III",
-                "PNY PCI-E Quadro RTX 6000 24GB DDR6");
+        computePart = new ComputePart[]
+                {new ComputePart("HDD"),
+                        new ComputePart("MotherBoard"),
+                        new ComputePart("CPU"),
+                        new ComputePart("Video Card")};
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "computePart=" + Arrays.toString(computePart) +
+                ", price=" + price +
+                ", computeDevice=" + Arrays.toString(computeDevice) +
+                '}';
     }
 }
-
 
 
 class ComputerDevice {
@@ -49,40 +53,26 @@ class ComputerDevice {
         this.device_Name = device_Name;
     }
 
-    public void setDevice_Name(String device_Name) {
-        this.device_Name = device_Name;
-    }
 
     @Override
     public String toString() {
-        return "ComputeDevice{" +
+        return "ComputerDevice{" +
                 "device_Name='" + device_Name + '\'' +
                 '}';
     }
 }
 
 class ComputePart {
-    protected String motherboard_Name;
-    protected String CPU_Name;
-    protected String HDD_Name;
-    protected String video_Card_Name;
+    protected String Computer_Part;
 
     @Override
     public String toString() {
         return "ComputePart{" +
-                "motherboard_Name='" + motherboard_Name + '\'' +
-                ", CPU_Name='" + CPU_Name + '\'' +
-                ", HDD_Name='" + HDD_Name + '\'' +
-                ", video_Card_Name='" + video_Card_Name + '\'' +
+                "Computer_Part='" + Computer_Part + '\'' +
                 '}';
     }
 
-    public ComputePart(String motherboard_Name, String CPU_Name, String HDD_Name, String video_Card_Name) {
-        this.motherboard_Name = motherboard_Name;
-        this.CPU_Name = CPU_Name;
-        this.HDD_Name = HDD_Name;
-        this.video_Card_Name = video_Card_Name;
+    public ComputePart(String computer_Part) {
+        Computer_Part = computer_Part;
     }
-
-
 }
---
+++ src/Module2/Lessons8/Task2.java
@@ -0,0 +1,5 @@
+package Module2.Lessons8;
+
+public class Task2 {
+    static private String regexTask = "12 var3 34 2%^i 90";
+}
