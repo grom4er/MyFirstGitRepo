361d816ce2e2463e7434b4b25e61e22c879df177
Tonitrus <grom4er@gmail.com>
2020-10-16 18:57:07 +0300

    Lesson 12 add task1 and task2

--- src/Module2/Lessons12/Task1.java
+++ src/Module2/Lessons12/Task1.java
@@ -1,15 +1,18 @@
 package Module2.Lessons12;
 
 class Triple<T1, T2, T3> {
-    Triple(T1 t1, T2 t2, T3 t3 ){
-        first = t1;
-        second = t2;
-        third = t3;
-    };
-
     T1 first;
     T2 second;
     T3 third;
+
+    Triple(T1 t1, T2 t2, T3 t3) {
+        first = t1;
+        second = t2;
+        third = t3;
+    }
+
+    ;
+
 
     @Override
     public String toString() {
---
+++ src/Module2/Lessons12/Task2.java
@@ -0,0 +1,11 @@
+package Module2.Lessons12;
+
+import java.util.List;
+
+public class Task2 {
+    static <T> void fill(List<T> list, T value) {
+        for (int i = 0; i < list.size(); ++i) {
+            list.set(i, value);
+        }
+    }
+}
